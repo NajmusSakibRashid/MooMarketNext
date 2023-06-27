f0dc3c76eb0ee90c3e41f18193aa8208253bd723
Najmus Sakib Rashid <1805066@ugrad.cse.buet.ac.bd>
2023-06-27 17:03:14 +0600

    api experiment

--- app/api/[collection]/route.js
+++ app/api/[collection]/route.js
@@ -1,13 +1,46 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
-export const GET=async (request,{params})=>{
-    const client=await clientPromise
-    const db=client.db("MooMarket")
-    const bestDeal=await db
-        .collection(params.collection)
-        .find({})
-        .sort({"id":1})
-        .toArray()
-    return NextResponse.json(bestDeal)
+export const GET = async (request, { params }) => {
+    // const client=await clientPromise
+    // const db=client.db("MooMarket")
+    // const data=await db
+    //     .collection(params.collection)
+    //     .find({})
+    //     .sort({"id":1})
+    //     .toArray()
+    return NextResponse.json([
+        {
+            id: 0,
+            specifications: [
+                "specification 1"
+            ],
+            image: 'cow1.jpg',
+            title: 'Cow'
+        },
+        {
+            id: 0,
+            specifications: [
+                "specification 1"
+            ],
+            image: 'cow1.jpg',
+            title: 'Cow'
+        },
+        {
+            id: 0,
+            specifications: [
+                "specification 1"
+            ],
+            image: 'cow1.jpg',
+            title: 'Cow'
+        },
+        {
+            id: 0,
+            specifications: [
+                "specification 1"
+            ],
+            image: 'cow1.jpg',
+            title: 'Cow'
+        }
+    ])
 }
